92f865abe2c3e2bd401bb3bff91208914521f6c8
tecinfopi <tecinfopi@yahoo.com>
2024-02-06 22:13:40 -0300

    Projeto Cadastro

---
+++ Projeto_Pesquisa.dpr
@@ -0,0 +1,14 @@
+program Projeto_Pesquisa;
+
+uses
+  Vcl.Forms,
+  uprincipal in 'uprincipal.pas' {Frmprincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmprincipal, Frmprincipal);
+  Application.Run;
+end.
